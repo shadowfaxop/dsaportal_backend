9f4a1baba668f1084d430754d14a7b0fffb432d0
omkar_nikam <omkar.nikam-v@adityabirlacapital.com>
2024-04-04 20:42:54 +0530

    commit to patch01 ap.py and requirement

--- app.py
+++ app.py
@@ -1,28 +1,43 @@
-# app.py
-import falcon
-import json
-
-
-class SubmitResource:
-    def on_post(self, req, resp):
-        data = json.loads(req.stream.read().decode('utf-8'))
-        value1 = int(data['value1'])
-        value2 = int(data['value2'])
-        value3 = int(data['value3'])
-        value4 = int(data['value4'])
-        result = value1 + value2 + value3 + value4
-        # Store the result in some database or file
-        resp.media = {'result': result}
-
-
-class ViewResource:
-    def on_get(self, req, resp):
-        # Retrieve the result from the database or file
-        # For simplicity, just return a placeholder value
-        result = 100
-        resp.media = {'result': result}
-
-
-api = falcon.API()
-api.add_route('/submit', SubmitResource())
-api.add_route('/view', ViewResource())
+from datetime import datetime,date
+import os
+import base64
+import pymysql
+from pymysql import converters
+import pandas as pd
+import mysql.connector as db
+ 
+ 
+HOST="widget-dev.c8mrfyuk9eoc.ap-south-1.rds.amazonaws.com"
+USERNAME="posdev"
+PASSWORD="FAT4NMH%40ZWVTkt8"
+DATABASE_NAME="posdev"
+ 
+def connect_B2B_db():
+    converions = converters.conversions
+    converions[pymysql.FIELD_TYPE.DATE] = lambda x: str(x)
+    converions[pymysql.FIELD_TYPE.DATETIME] = lambda x: str(x)
+    converions[pymysql.FIELD_TYPE.TIMESTAMP] = lambda x: str(datetime.strptime(x,"%Y-%m-%d %H:%M:%S").strftime("%Y-%m-%d %H:%M:%S"))
+    converions[pymysql.FIELD_TYPE.LONGLONG] = lambda x: str(x)
+    return pymysql.connect(HOST, USERNAME, PASSWORD, DATABASE_NAME,conv=converions)
+ 
+def execute_query(query, dBtype):
+    try:
+        if dBtype == "B2B":
+            conn=connect_B2B_db()
+                       
+        cursor=conn.cursor(pymysql.cursors.DictCursor)
+        cursor.execute(query)
+        return True, cursor
+    except Exception as e:
+        print(e)
+        return False, None
+    finally:
+        conn.close()
+ 
+main_query = f'''
+select now();
+'''
+print("Executing MAIN QUERY ... ",datetime.now().strftime("%H:%M:%S"))
+success,result_main=execute_query(main_query, "POS")
+excel_main = pd.DataFrame(result_main)
+print(excel_main)
